d3bc725df768be31ea9094ff2ea51dc96a855059
nlbucurur <nlbucurur@unal.edu.co>
2021-02-02 21:06:29 -0500

    Add Documents and the first code

---
+++ Beginner/Day1-WorkingWithVariables/Day-1-Printing-Start.py
@@ -0,0 +1,22 @@
+"""Day 1 -  working with variables."""
+
+# imprimir cosas
+print("Hello World!")
+
+print("Day 1 - Python Print Function")
+
+print("The function is declared like this")
+
+print("print('what to print')")
+
+print("Hello World! \nit's a great day")
+
+print("Hello World!" + " it's a great day")
+
+# Pedir información
+input("What is your name? ")
+
+# Aquí todo va de adentro hacia afuera, así que va a hacer lo siguiente.
+# Pregunta primero el nombre
+#luego dice Hello + el nombre.
+print("Hello " + input("What is your name? ") + "!")
